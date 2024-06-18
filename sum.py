8b7391b208e5535aa113335c36f024efad4236ae
DavideNocera00 <163406000+DavideNocera00@users.noreply.github.com>
2024-06-18 16:23:40 +0200

    Update sum.py

--- sum.py
+++ sum.py
@@ -36,14 +36,14 @@ time.sleep(res)
 url = f"http://localhost:8084/api/resources/updateAvailability/{param}"
 
 # Send the PUT request without JSON payload
-#response = requests.put(url, headers=headers)
+response = requests.put(url, headers=headers)
 
 # Check the response status code and print the response
-#if response.status_code == 200:
-#    print("Request was successful.")
-#else:
-#    print(f"Failed to send request. Status code: {response.status_code}")
+if response.status_code == 200:
+    print("Request was successful.")
+else:
+    print(f"Failed to send request. Status code: {response.status_code}")
 
-#print("Response content:", response.content.decode())
+print("Response content:", response.content.decode())
 
 sys.exit(0)
